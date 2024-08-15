989fb67f1d991d8f1574d6eb3640d67f85e5bbe6
nekosanti <110539710+nekosanti@users.noreply.github.com>
2024-08-15 16:20:17 -0300

    Version Final

--- Ejercicio_3.py
+++ Ejercicio_3.py
@@ -1,6 +1,20 @@
-with open('archivo_texto3a.txt', 'r+') as file:
+nombres_a = []
+nombres_b = []
+
+with open('archivo_texto3a.txt', 'r') as file:
     lines = file.readlines()
     for line in lines:
-        nombres_a = [line.strip("\n")]
+        nombres_a.append(line[:-1])
 
-print(nombres_a)
+with open('archivo_texto3b.txt', 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        nombres_b.append(line[:-1])
+
+nombres_c = [nombres_a[0] + nombres_b[0], nombres_a[1] + nombres_b[1], nombres_a[2] + nombres_b[2], nombres_a[3] + nombres_b[3]]
+
+print(nombres_c)
+
+with open('archivo_texto3b.txt', 'w+') as file:
+    for nombre in nombres_c:
+        file.write(nombre + "\n")
