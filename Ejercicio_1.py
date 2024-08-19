7cb87290ce2901de21541316556f3df6cea79e21
FranciscoNG2007 <127147725+FranciscoNG2007@users.noreply.github.com>
2024-08-19 07:43:53 -0300

    Update Ejercicio_1.py

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -1,1 +1,9 @@
 
+with open( 'archivo_texto1.txt', 'r') as file:
+    lines = file.readlines()  
+    cantidad_lineas = len(lines) 
+
+    for line in lines:
+           print(line.strip())  
+        
+    print(f"\nLa cantidad de líneas en el archivo es: {cantidad_lineas}")
